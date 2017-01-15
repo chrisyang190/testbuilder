30770b2d90f8b4e9a4ff26eb6e707764656d3d9f
Chris Yang <chrisyang190@gmail.com>
2017-01-14 16:05:56 -0800

    Step one: Add conditions for Diner's club and American Express to detectNetwork.js

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,8 +11,18 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+var cardArray = cardNumber.split("").map(function(s){
+	return parseInt(s);
+});
 
+if (cardArray[0]==3 && (cardArray[1]==8 || cardArray[1]==9) && cardArray.length ==14) {
+	return "Diner's Club";
+} else if (cardArray[0]==3 && (cardArray[1]==4 || cardArray[1]==7) && cardArray.length ==15) {
+	return "American Express";
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+
+}
 };
 
-
+//console.log(detectNetwork('373456789012345'));
